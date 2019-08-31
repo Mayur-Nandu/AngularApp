4dd497b1cf580f7fe4c170a3393909eb79e96e6d
mayurnandu <nandumayur95@gmail.com>
2019-08-31 16:19:52 +0530

    week 3 Assignment Task2

--- angularapp/confusionapp/src/app/dishdetail/dishdetail.component.ts
+++ angularapp/confusionapp/src/app/dishdetail/dishdetail.component.ts
@@ -1,26 +1,111 @@
-import { Component, OnInit,Input } from '@angular/core';
+import { Component, OnInit,Input,ViewChild } from '@angular/core';
 import { Dish } from '../shared/dish';
 import { DishService } from '../services/dish.service';
 
 import { Params, ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 import { switchMap } from 'rxjs/operators';
+import { FormBuilder, FormGroup,Validators, } from '@angular/forms';
+
 
 @Component({
   selector: 'app-dishdetail',
   templateUrl: './dishdetail.component.html',
   styleUrls: ['./dishdetail.component.scss']
 })
+
+
+
+
 export class DishdetailComponent implements OnInit {
  
   dishIds: string[];
   prev: string;
   next: string;
   dish: Dish;
+  CommentForm : FormGroup;
+  @ViewChild('commentform') CommentFormDirective;
+  cfVariable : {
+    author :'';
+    rating : '' ;
+    comment: '';
+   };
+   commentFormErrors ={
+    'author' :'',
+    'rating' : '' ,
+    'comment': '',
+   } ;
+
+   validationMessages = {
+    'author': {
+      'required':      'Author Name is required.',
+      'minlength':     'Author Name must be at least 2 characters long.',
+      'maxlength':     'Author Name cannot be more than 25 characters long.'
+    },
+    'comment': {
+      'required':      'Comment  is required.',
+      'minlength':     'Comment must be at least 2 characters long.',
+      
+    },
+  };
+
 
   constructor(private dishservice: DishService,
     private route: ActivatedRoute,
-    private location: Location) { }
+    private location: Location,
+    private cf : FormBuilder
+    ) { 
+      this.createFromComment();
+    }
+
+    createFromComment()
+    {
+      this.CommentForm = this.cf.group({
+        author : ['', [Validators.required,Validators.minLength(2),Validators.maxLength(25)] ],
+        rating :['', Validators.required ] ,
+        comment :['', [Validators.required, Validators.minLength(2)] ]
+      });
+
+      this.CommentForm.valueChanges
+      .subscribe(data => this.onValueChanged(data));
+
+      this.onValueChanged(); // (re)set validation messages now
+    }
+
+    onValueChanged(data?: any) {
+      
+      if (!this.CommentForm) { return; }
+      const form = this.CommentForm;
+      
+      for (const field in this.commentFormErrors) {
+        
+        if (this.commentFormErrors.hasOwnProperty(field)) {
+        
+          // clear previous error message (if any)
+          this.commentFormErrors[field] = '';
+          const control = form.get(field);
+          if (control && control.dirty && !control.valid) {
+        
+            const messages = this.validationMessages[field];
+            
+            for (const key in control.errors) {
+        
+              if (control.errors.hasOwnProperty(key)) {
+                this.commentFormErrors[field] += messages[key] + ' ';
+                
+              }
+            }
+          }
+        }
+      }
+    }
+
+
+    onSubmit() {
+      this.cfVariable = this.CommentForm.value;
+      console.log(this.cfVariable);
+      this.CommentFormDirective.resetForm();
+    }
 
   ngOnInit() {
     // const id = this.route.snapshot.params['id'];
