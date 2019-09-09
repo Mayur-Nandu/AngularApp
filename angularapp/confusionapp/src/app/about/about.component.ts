bda366792b2b12115492ae2e5f297dd2252a3bb4
Mayur-Nandu <nandumayur95@gmail.com>
2019-09-09 22:05:22 +0530

    Assignment 4 Task2

--- angularapp/confusionapp/src/app/about/about.component.ts
+++ angularapp/confusionapp/src/app/about/about.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit,Inject } from '@angular/core';
 import { Leader } from '../shared/leader';
 import { LeaderService } from '../services/leader.service';
 import { flyInOut,expand } from '../animations/app.animation';
+
+
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
@@ -18,11 +20,14 @@ import { flyInOut,expand } from '../animations/app.animation';
 export class AboutComponent implements OnInit {
 
   leaders:Leader[];
-  constructor(private leaderservice : LeaderService, @Inject('BaseURL') private BaseURL) { }
+  errmess:string;
+  constructor(private leaderservice : LeaderService, 
+    @Inject('BaseURL') private BaseURL) { }
 
   ngOnInit() {
     this.leaderservice.getLeaders().subscribe(
-      leaders => this.leaders = leaders
+      leaders => this.leaders = leaders,
+      errmess => this.errmess= <any> errmess 
     );
   }
 
--- angularapp/confusionapp/src/app/services/leader.service.ts
+++ angularapp/confusionapp/src/app/services/leader.service.ts
@@ -1,6 +1,5 @@
 import { Injectable,Inject } from '@angular/core';
 import { Observable,of } from 'rxjs';
-import { delay } from 'rxjs/operators';
 import { Leader } from '../shared/leader';
 import { map, catchError } from 'rxjs/operators';
 import { HttpClient } from '@angular/common/http';
